5bb063673871218bf67775ce09f5dedd5da69c63
Osam Ullah <141023064+osamullah420@users.noreply.github.com>
2024-12-10 23:28:26 +0500

    changes
    changes

--- client/src/components/home/CompaniesTestomonial.jsx
+++ client/src/components/home/CompaniesTestomonial.jsx
@@ -58,6 +58,29 @@ const CompaniesTestimonial = () => {
     slidesToScroll: 1,
     nextArrow: <simpleNextArrow />,
     prevArrow: <simplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   
   };
 
