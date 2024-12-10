b92bafaf85213cdc673ab1f7bff3fbe96d69f22c
Shahzad Arif <shahzadarif545@gmail.com>
2024-12-10 22:30:57 +0500

    ALLsoftware categories fixed

--- client/src/components/home/AllSoftwareCategories.jsx
+++ client/src/components/home/AllSoftwareCategories.jsx
@@ -25,7 +25,7 @@ const AllSoftwareCategories = () => {
 			<h2 className="text-2xl font-bold text-center mb-8">
 				Browse all Software Categories
 			</h2>
-			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4 md:px-10">
+			<div className="grid grid-cols-1 sm:grid-cols-2  gap-6 px-4 md:px-10">
 				{categories.map((category, index) => (
 					<div key={index}>
 						<Dropdown
