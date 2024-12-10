f44968849840ef296e9fc9c6783c4d57731dda1c
Shahzad Arif <shahzadarif545@gmail.com>
2024-12-10 20:28:15 +0500

    Pushing

--- client/src/components/cards/SoftwareCard.jsx
+++ client/src/components/cards/SoftwareCard.jsx
@@ -1,64 +1,64 @@
 import React, { useState, useEffect } from "react";
 
 const SoftwareCard = ({ icon, title, tekponScore, activeIndex, index }) => {
-  const [progress, setProgress] = useState((tekponScore / 10) * 100); // Set initial progress based on score
+	const [progress, setProgress] = useState((tekponScore / 10) * 100); // Set initial progress based on score
 
-  useEffect(() => {
-    if (activeIndex !== null) {
-      let progressValue = 0;
-      const targetValue = (tekponScore / 10) * 100;
+	useEffect(() => {
+		if (activeIndex !== null) {
+			let progressValue = 0;
+			const targetValue = (tekponScore / 10) * 100;
 
-      // Animate all progress bars from 0 to target value
-      const interval = setInterval(() => {
-        if (progressValue < targetValue) {
-          progressValue += 1; // Increment progress by 1 every 5ms
-          setProgress(progressValue);
-        } else {
-          clearInterval(interval); // Stop when the progress reaches the target value
-        }
-      }, 5); // Adjust the speed of animation with interval
+			// Animate all progress bars from 0 to target value
+			const interval = setInterval(() => {
+				if (progressValue < targetValue) {
+					progressValue += 1; // Increment progress by 1 every 5ms
+					setProgress(progressValue);
+				} else {
+					clearInterval(interval); // Stop when the progress reaches the target value
+				}
+			}, 5); // Adjust the speed of animation with interval
 
-      // Cleanup the interval on component unmount or when activeIndex changes
-      return () => clearInterval(interval);
-    }
-  }, [activeIndex, tekponScore]);
+			// Cleanup the interval on component unmount or when activeIndex changes
+			return () => clearInterval(interval);
+		}
+	}, [activeIndex, tekponScore]);
 
-  return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden mt-2 px-4 py-6 max-w-sm mx-2">
-      <div className="flex items-center space-x-8 -my-4">
-        {/* Icon */}
-        <div>
-          <img src={icon} alt={title} className="h-22 w-22" />
-        </div>
+	return (
+		<div className="bg-white shadow-lg rounded-lg overflow-hidden mt-2 px-4 py-6 max-w-sm mx-4">
+			<div className="flex items-center space-x-8 -my-4">
+				{/* Icon */}
+				<div>
+					<img src={icon} alt={title} className="h-22 w-22" />
+				</div>
 
-        {/* Title and Tekpon Score */}
-        <div>
-          <h3 className="text-custom-gradient text-xl font-bold mb-1 truncate">
-            {title}
-          </h3>
-          <div className="flex flex-row space-x-10">
-            <p className="text-gray-900 mb-2">Tekpon Score</p>
-            <p>{tekponScore}/10</p>
-          </div>
+				{/* Title and Tekpon Score */}
+				<div>
+					<h3 className="text-custom-gradient text-xl font-bold mb-1 truncate">
+						{title}
+					</h3>
+					<div className="flex flex-row space-x-10">
+						<p className="text-gray-900 mb-2">Tekpon Score</p>
+						<p>{tekponScore}/10</p>
+					</div>
 
-          {/* Progress Bar with Dot */}
-          <div className="relative w-full h-0.5 bg-gray-200 rounded-full">
-            {/* Colored Line Up to Dot */}
-            <div
-              className="absolute top-0 left-0 h-0.5 bg-[#ff0800] rounded-full"
-              style={{ width: `${progress}%` }} // Animate the width of the bar
-            ></div>
+					{/* Progress Bar with Dot */}
+					<div className="relative w-full h-0.5 bg-gray-200 rounded-full">
+						{/* Colored Line Up to Dot */}
+						<div
+							className="absolute top-0 left-0 h-0.5 bg-[#ff0800] rounded-full"
+							style={{ width: `${progress}%` }} // Animate the width of the bar
+						></div>
 
-            {/* Dot */}
-            <div
-              className="absolute top-1/2 transform -translate-y-1/2 w-3 h-3 bg-[#ff0800] rounded-full shadow-md"
-              style={{ left: `${progress}%` }} // Position the dot based on progress
-            ></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+						{/* Dot */}
+						<div
+							className="absolute top-1/2 transform -translate-y-1/2 w-3 h-3 bg-[#ff0800] rounded-full shadow-md"
+							style={{ left: `${progress}%` }} // Position the dot based on progress
+						></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default SoftwareCard;
--- client/src/components/home/PopularSoftwareCategories.jsx
+++ client/src/components/home/PopularSoftwareCategories.jsx
@@ -1,116 +1,115 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import SoftwareCard from "../cards/SoftwareCard";
-
 import freshdeskIcon from "/Freshdesk-favicon.webp";
 
 const PopularSoftwareCategories = () => {
-  const [activeIndex, setActiveIndex] = useState(null);
-   const getCategoryName = () => {
-    return activeIndex !== null
-      ? categorySidebarItems[activeIndex]
-      : "Help Desk"; // Default if no category is selected
-  };
+	const [activeIndex, setActiveIndex] = useState(null);
+	const getCategoryName = () => {
+		return activeIndex !== null
+			? categorySidebarItems[activeIndex]
+			: "Help Desk"; // Default if no category is selected
+	};
 
-  const categories = [
-    {
-      icon: freshdeskIcon,
-      title: "Freshdesk",
-      tekponScore: 9.2,
-    },
-    {
-      icon: freshdeskIcon,
-      title: "Freshdesk",
-      tekponScore: 9.1,
-    },
-    {
-      icon: freshdeskIcon,
-      title: "Freshdesk",
-      tekponScore: 9.0,
-    },
-    {
-      icon: freshdeskIcon,
-      title: "Freshdesk",
-      tekponScore: 6,
-    },
-    {
-      icon: freshdeskIcon,
-      title: "Freshdesk",
-      tekponScore: 9.2,
-    },
-    {
-      icon: freshdeskIcon,
-      title: "Freshdesk",
-      tekponScore: 9.1,
-    },
-  ];
+	const categories = [
+		{
+			icon: freshdeskIcon,
+			title: "Freshdesk",
+			tekponScore: 9.2,
+		},
+		{
+			icon: freshdeskIcon,
+			title: "Freshdesk",
+			tekponScore: 9.1,
+		},
+		{
+			icon: freshdeskIcon,
+			title: "Freshdesk",
+			tekponScore: 9.0,
+		},
+		{
+			icon: freshdeskIcon,
+			title: "Freshdesk",
+			tekponScore: 6,
+		},
+		{
+			icon: freshdeskIcon,
+			title: "Freshdesk",
+			tekponScore: 9.2,
+		},
+		{
+			icon: freshdeskIcon,
+			title: "Freshdesk",
+			tekponScore: 9.1,
+		},
+	];
 
-  const categorySidebarItems = [
-    "CRM",
-    "Field Service Management",
-    "Help Desk",
-    "Inventory Management",
-    "Landing Page",
-    "Payroll",
-    "Project Management",
-    "Subscription Management",
-  ];
+	const categorySidebarItems = [
+		"CRM",
+		"Field Service Management",
+		"Help Desk",
+		"Inventory Management",
+		"Landing Page",
+		"Payroll",
+		"Project Management",
+		"Subscription Management",
+	];
 
-  return (
-    <div className="bg-gray-100 py-12">
-      <div className="mx-auto px-20">
-        <h2 className="text-4xl font-bold mb-4 text-center">
-          Popular Software Categories
-        </h2>
-        <h2 className="text-center mb-10 text-xl">
-          Discover the best software options tailored to your business needs.
-        </h2>
-        <div className="flex flex-row items-start justify-center space-x-6">
-          {/* Sidebar */}
-          <div className="mt-6 rounded-lg w-1/5">
-            <ul>
-              {categorySidebarItems.map((item, index) => (
-                <li
-                  key={index}
-                  onClick={() => setActiveIndex(index)} // Set the clicked item as active
-                  className={`font-bold text-xl cursor-pointer py-2 px-4 rounded-lg ${
-                    activeIndex === index
-                      ? "color-custom-gradient text-white" // Active item styles
-                      : "text-gray-900" // Default styles
-                  }`}
-                >
-                  {item}
-                </li>
-              ))}
-            </ul>
-          </div>
+	return (
+		<div className="bg-gray-100 py-8 md:py-12">
+			<div className="container mx-auto px-4 md:px-6 lg:px-20">
+				<h2 className="text-2xl md:text-4xl font-bold mb-4 text-center">
+					Popular Software Categories
+				</h2>
+				<h2 className="text-center mb-8 md:mb-10 text-base md:text-xl text-gray-600">
+					Discover the best software options tailored to your business needs.
+				</h2>
+				<div className="flex flex-col lg:flex-row items-start justify-center space-y-6 lg:space-y-0 lg:space-x-6">
+					{/* Sidebar */}
+					<div className="w-full lg:w-1/5 bg-white rounded-lg shadow-md">
+						<ul className="grid grid-cols-2 lg:grid-cols-1 gap-2 p-4">
+							{categorySidebarItems.map((item, index) => (
+								<li
+									key={index}
+									onClick={() => setActiveIndex(index)}
+									className={`font-bold text-sm md:text-xl cursor-pointer py-2 px-4 rounded-lg text-center lg:text-left ${
+										activeIndex === index
+											? "color-custom-gradient text-white"
+											: "text-gray-900 hover:bg-gray-100"
+									}`}
+								>
+									{item}
+								</li>
+							))}
+						</ul>
+					</div>
 
-          {/* Grid */}
-          <div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-              {categories.map((category, index) => (
-                <SoftwareCard
-                  key={index}
-                  icon={category.icon}
-                  title={category.title}
-                  tekponScore={category.tekponScore}
-                  activeIndex={activeIndex} // Pass activeIndex to each card
-                  index={index}
-                />
-              ))}
-            </div>
-            <div className="text-center mt-14">
-              <a
-                href="#"
-                className="text-purple-600 font-medium text-xl hover:text-purple-800 underline"
-              >
-                See All {getCategoryName()} Software
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+					{/* Grid */}
+					<div className="w-full lg:w-4/5">
+						<div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+							{categories.map((category, index) => (
+								<SoftwareCard
+									key={index}
+									icon={category.icon}
+									title={category.title}
+									tekponScore={category.tekponScore}
+									activeIndex={activeIndex}
+									index={index}
+								/>
+							))}
+						</div>
+						<div className="text-center mt-8 md:mt-14">
+							<a
+								href="#"
+								className="text-purple-600 font-medium text-base md:text-xl hover:text-purple-800 underline"
+							>
+								See All {getCategoryName()} Software
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default PopularSoftwareCategories;
