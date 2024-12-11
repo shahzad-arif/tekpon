109b25e274adc230fe3797139731be97464c3607
Shahzad Arif <shahzadarif545@gmail.com>
2024-12-11 15:18:57 +0500

    Fixed cards for the mobile

--- client/src/components/cards/CompanyReview.jsx
+++ client/src/components/cards/CompanyReview.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 
 const CompanyReview = ({ data }) => {
-  return (
-    <div className="p-6 bg-white shadow-xl rounded-lg mx-4 flex flex-col">
-      <div className="flex flex-row items-center justify-center space-x-4">
-        <div>
-          <img
-            src={data.image}
-            alt={data.name}
-            className="w-16 h-16 rounded-full mb-2"
-          />
-        </div>
-        <div>
-          <h3 className="text-lg font-bold text-center">{data.name}</h3>
-          <p className="text-sm text-gray-500 text-center">{data.title}</p>
-        </div>
-      </div>
-      <div>
-        <p className="text-gray-700 text-center mt-4">{data.quote}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="p-6 bg-white shadow-xl rounded-lg mx-4 flex flex-col">
+			<div className="flex flex-col sm:flex-row items-center justify-center space-x-4">
+				<div>
+					<img
+						src={data.image}
+						alt={data.name}
+						className="w-16 h-16 rounded-full mb-2"
+					/>
+				</div>
+				<div>
+					<h3 className="text-lg font-bold text-center">{data.name}</h3>
+					<p className="text-sm text-gray-500 text-center">{data.title}</p>
+				</div>
+			</div>
+			<div>
+				<p className="text-gray-700 text-center mt-4">{data.quote}</p>
+			</div>
+		</div>
+	);
 };
 
 export default CompanyReview;
