9a2507eed72c1ccc2ff1f3f649841fd09969d267
Shahzad Arif <shahzadarif545@gmail.com>
2024-12-11 12:52:27 +0500

    Minor Changes

--- client/src/components/home/AllSoftwareCategories.jsx
+++ client/src/components/home/AllSoftwareCategories.jsx
@@ -1,23 +1,22 @@
 import React, { useState } from "react";
-import Dropdown from "../common/Dropdown"; // Import the reusable Dropdown component
+import Dropdown from "../common/Dropdown";
 import { categories } from "../../text/homePageText/homeText";
 
 const AllSoftwareCategories = () => {
 	const [activeDropdown, setActiveDropdown] = useState(null);
 
 	const handleDropdownToggle = (index) => {
-		// Toggle the clicked dropdown; close others
 		setActiveDropdown((prevIndex) => (prevIndex === index ? null : index));
 	};
 
 	return (
-		<section className="bg-gray-50 py-10">
+		<section className="bg-gray-50 py-10 overflow-x-scroll">
 			<h2 className="text-2xl font-bold text-center mb-8">
 				Browse all Software Categories
 			</h2>
-			<div className="grid grid-cols-1 sm:grid-cols-2  gap-6 px-4 md:px-10">
+			<div className="grid grid-cols-1 sm:grid-cols-2 gap-6 px-4 md:px-10 overflow-visible">
 				{categories.map((category, index) => (
-					<div key={index}>
+					<div key={index} className="relative overflow-visible">
 						<Dropdown
 							title={category.title}
 							items={category.items}
